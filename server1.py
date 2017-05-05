d1f0a8f518272b26c986fdc9a500ab50a99fbdaa
Bonnie <kwongb@flip1.engr.oregonstate.edu>
2017-05-04 21:17:28 -0700

    changing to update port num in command line

--- server1.py
+++ server1.py
@@ -2,17 +2,21 @@
 
 # import the socket module
 import socket
+import sys
 
 #create socket object
 # s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s = socket.socket()
 print s
 
-host = socket.gethostname()
-#host = "127.0.0.1"
-# get a port number
-port = 50050
+#host = socket.gethostname()
+host = "127.0.0.1"
 
+
+# get a port number
+#port = 50050
+port = int(sys.argv[1])
+print sys.argv[1] + " is the port to use"
 
 # bind to the port
 s.bind((host, port))
