40bf153d576dd0736363489a4adfaac33486d692
Bonnie <kwongb@flip1.engr.oregonstate.edu>
2017-05-06 23:58:11 -0700

    working, if client closes, server reopens to listen for new connections

--- server1.py
+++ server1.py
@@ -26,37 +26,40 @@ port = int(sys.argv[1])
 # bind to the port
 s.bind((host, port))
 
-s.listen(5)
 
-# take out this line from while loop
-connection, addr = s.accept()
-print 'Connection from', addr[0]
-
+# first while loop surrounds listem
 while True:
- #connection, addr = s.accept()
- 
- # get what the client entered
- msg = connection.recv(4096)
- # if the msg is quit, it means that client is going to close connectoin
- if msg == "quit\n":
-  print "client is quiting"
+  s.listen(5)
 
- print msg
- 
+  print "Listening for a new connection..."
 
 
- # get string from user
- response = raw_input("Enter a reply to client:")
+  # take out this line from while loop
+  connection, addr = s.accept()
+  print 'Connection from', addr[0]
+
+  while True:
+   #connection, addr = s.accept()
  
- #print 'got a connection', addr
- #c.send("got a connection")
- try:
-  n = connection.send(response)
-  print n
- except socket.error:
-  print "you have an error sending. conn is broken"
-  sys.exit(1)
- # seems like if n equals 0, it could not send, so connection closed (with quit):
+   #  get what the client entered
+   msg = connection.recv(4096)
+   # if the msg is quit, it means that client is going to close connectoin
+   if msg == "quit\n":
+    print "client is quiting"
+    break  # creak out of while loop. 
+   print msg
+   # get string from user
+   response = raw_input("Enter a reply to client:")
+ 
+   #print 'got a connection', addr
+   #c.send("got a connection")
+   try:
+    n = connection.send(response)
+    print n
+   except socket.error:
+    print "you have an error sending. conn is broken"
+    sys.exit(1)
+   # seems like if n equals 0, it could not send, so connection closed (with quit):
 
 connection.close()
 
