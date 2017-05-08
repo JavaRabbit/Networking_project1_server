dd441dbf35433852aec85d9653a14dd110f40fe6
Bonnie <kwongb@flip1.engr.oregonstate.edu>
2017-05-07 20:04:35 -0700

    finished modulizing python

--- server1.py
+++ server1.py
@@ -17,9 +17,8 @@ import sys
 # global variable socket
 s = socket.socket()
 
-# global handle
 
-
+####################################
 # Function to verify that command line arguments are correct
 def verifyNumArguments():
   # if no port number is given, show usage and exit
@@ -27,6 +26,7 @@ def verifyNumArguments():
     print "Usage: ./chatserve <portnumber>"
     exit(1)
 
+####################################
 # Function to initialize socket connection
 def initializeConnection():
 
@@ -40,7 +40,7 @@ def initializeConnection():
   s.bind((host, port))
 
 
-
+################################
 # Get a handle form the user
 def getHandle():
   serverHandle = raw_input("Enter a handle name for this server:")
@@ -48,56 +48,66 @@ def getHandle():
   fullHandle = serverHandle + " >" 
 
 
+
+###################################
+#  chat function runs a while loop to 
+#  receive and send messages to clients
+#  the outer while loop listens for connections
+# thus if a connection terminates
+# it is able to use the same socket
+# to connect to new connections
+#
+####################################
+def chat():
+  # first while loop surrounds listem
+  while True:
+    s.listen(5)
+
+    print "Listening for a new connection..."
+
+    # accept a new connection from client
+    connection, addr = s.accept()
+    print 'Connection from', addr[0]
+
+    while True:
+ 
+     #  get what the client entered
+     msg = connection.recv(4096)
+     # if the msg is quit, it means that client is going to close connectoin
+     if msg == "quit\n":
+      print "Client has decided to terminate connection. This connection will now close.\n"
+      break  # creak out of while loop. 
+   
+     #  else the client did not want to quit, so get a response from the server`
+     print msg
+     # get string from user on server side
+     response = raw_input(fullHandle)
+ 
+     # enclose the send method in a try exception, in case there is
+     # a socket error
+     try:
+      n = connection.send(fullHandle + response  )
+    
+      # if server user decides to quit, send message to client, then close this connection
+      if(response == "quit"):
+        connection.close()
+        print "Now closing this connection\n"
+        break  # break out of this loop to wait for new connection
+     except socket.error:
+      print "you have an error sending. conn is broken"
+      sys.exit(1)
+
+  connection.close()
+
 # verify user entered correctnumber of arguments
 verifyNumArguments()
+
+# get a handle from the user
+getHandle()
 
 # initialize socket
 initializeConnection()
 
-
-# Get a handle for the server
-#serverHandle = raw_input("Enter a name for this server:")
-
-getHandle()
-
-# first while loop surrounds listem
-while True:
-  s.listen(5)
-
-  print "Listening for a new connection..."
-
-
-  # take out this line from while loop
-  connection, addr = s.accept()
-  print 'Connection from', addr[0]
-
-  while True:
-   #connection, addr = s.accept()
- 
-   #  get what the client entered
-   msg = connection.recv(4096)
-   # if the msg is quit, it means that client is going to close connectoin
-   if msg == "quit\n":
-    print "Client has decided to terminate connection. This connection will now close.\n"
-    break  # creak out of while loop. 
-   
-   #  else the client did not want to quit, so get a response from the server`
-   print msg
-   # get string from user on server side
-   response = raw_input(fullHandle)
- 
-   try:
-    n = connection.send(fullHandle + response  )
-    
-    # if server user decides to quit, send message to client, then close this connection
-    if(response == "quit"):
-      connection.close()
-      print "Now closing this connection\n"
-      break  # break out of this loop to wait for new connection
-   except socket.error:
-    print "you have an error sending. conn is broken"
-    sys.exit(1)
-   # seems like if n equals 0, it could not send, so connection closed (with quit):
-
-connection.close()
+# call the chat function to call chat method
+chat()
 
