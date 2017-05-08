eea9f0cc755c9d975619bea03cfc2a5faf75b4de
Bonnie <kwongb@flip1.engr.oregonstate.edu>
2017-05-07 19:47:29 -0700

    working on modulizing python

--- readme.txt
+++ readme.txt
@@ -3,11 +3,23 @@ chmod +x chatserve
 
 before you can use the server app.
 
+
+
+
+SERVER  ***************************
+
 Usage for server is:
 ./chatserve  <portnum>
 
 example:
 ./chatserve 55500
+
+
+Usage for client is:
+
+./chatclient flip1.engr.oregonstate.edu <port number>
+(note that it can be flip1, flip2, etc. Whichever flip server the chatserver is running on)
+
 
 
 Notes: 
--- server1.py
+++ server1.py
@@ -1,31 +1,64 @@
 #!/user/bin/python
 
+# Project: Project 1 Networking CS 375
+# Program:  Chat Server 
+# Language: Python 2.7
+# Author: Bonnie Kwong
+# Description: a python chat server capable of 
+# chatting with a client
+# Date: May 7, 2017
+
+
+
 # import the socket module
 import socket
 import sys
 
-# if no port number is given, show usage and exit
-if len(sys.argv) != 2:
-  print "Usage: ./chatserve <portnumber>"
-  exit(1)
-
-#create socket object
-# s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# global variable socket
 s = socket.socket()
-print s
 
-host = socket.gethostname()
-#host = "127.0.0.1"
-print host
+# global handle
 
-# get a port number
-#port = 50050
-port = int(sys.argv[1])
-# print sys.argv[1] + " is the port to use"
 
-# bind to the port
-s.bind((host, port))
+# Function to verify that command line arguments are correct
+def verifyNumArguments():
+  # if no port number is given, show usage and exit
+  if len(sys.argv) != 2:
+    print "Usage: ./chatserve <portnumber>"
+    exit(1)
 
+# Function to initialize socket connection
+def initializeConnection():
+
+  host = socket.gethostname()
+  print host
+
+  # get a port number
+  port = int(sys.argv[1])
+
+  # bind to the port
+  s.bind((host, port))
+
+
+
+# Get a handle form the user
+def getHandle():
+  serverHandle = raw_input("Enter a handle name for this server:")
+  global fullHandle 
+  fullHandle = serverHandle + " >" 
+
+
+# verify user entered correctnumber of arguments
+verifyNumArguments()
+
+# initialize socket
+initializeConnection()
+
+
+# Get a handle for the server
+#serverHandle = raw_input("Enter a name for this server:")
+
+getHandle()
 
 # first while loop surrounds listem
 while True:
@@ -47,20 +80,19 @@ while True:
    if msg == "quit\n":
     print "Client has decided to terminate connection. This connection will now close.\n"
     break  # creak out of while loop. 
+   
+   #  else the client did not want to quit, so get a response from the server`
    print msg
-   # get string from user
-   response = raw_input("Enter a reply to client:")
+   # get string from user on server side
+   response = raw_input(fullHandle)
  
-   # if input from user is "quit", close this connection
-   if(response == "quit"):
-     print "you want to quit?"
-
    try:
-    n = connection.send(response)
-    # print n
+    n = connection.send(fullHandle + response  )
+    
     # if server user decides to quit, send message to client, then close this connection
     if(response == "quit"):
       connection.close()
+      print "Now closing this connection\n"
       break  # break out of this loop to wait for new connection
    except socket.error:
     print "you have an error sending. conn is broken"
